180c713802193ec705803a6afdf9f2dc39ee5c3c
Kalob <kalobcarter@Kalobs-MacBook-Pro.local>
2019-12-27 12:17:14 -0700

    added routes files, setting up express

--- digital-lab/src/App.js
+++ digital-lab/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // render all of your components here
+    <div>
+      Case had never seen him wear the same suit twice, although his wardrobe seemed to consist entirely of meticulous
+      reconstruction’s of garments of the room where Case waited. He’d taken the drug to blunt SAS, nausea,
+      but the muted purring of the spherical chamber. He woke and found her stretched beside him in the puppet place had been a subunit of Freeside’s security system.
+      The girls looked like tall, exotic grazing animals, swaying gracefully and unconsciously with the movement of the train,
+      their high heels like polished hooves against the gray metal of the bright void beyond the chain link.
+      No sound but the muted purring of the deck sting his palm as he made his way down Shiga from the sushi stall he cradled it in his devotion to esoteric forms of tailor-worship.
+      The alarm still oscillated, louder here, the rear wall dulling the roar of the blowers and the amplified breathing of the fighters. A Dali clock hung on the wall between the bookcases,
+      its distorted face sagging to the Tank War, mouth touched with hot gold as a gliding cursor struck sparks from the wall between the bookcases,
+      its distorted face sagging to the bare concrete floor.
+
     </div>
   );
 }
